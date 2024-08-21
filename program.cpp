c5f98391cc0208a675b78ffba15dd85da6ab5aa4
Fabio Ramos <fabiowilliam466@gmail.com>
2024-08-21 15:53:59 -0400

    teste

---
+++ program.cpp
@@ -0,0 +1,15 @@
+#include "stdio.h"
+
+
+// implementacao de funções de ordenação
+
+//colocar somente a etiqueta delas
+void bubbleSort(int *vetor, int tamanho);
+void insertionSort(int *vetor, int tamanho);
+void mergeSort(int *vetor, int inicio, int fim);
+void heapSort(int *vetor, int tamanho);
+void quickSort(int *vetor, int inicio, int fim);
+void countingSort(int *vetor, int tamanho);
+
+
+int inc, fim, stp, rpt;
