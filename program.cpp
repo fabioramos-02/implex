2ac7012434fe2d1357a4579886a1e18826cc958c
Fabio Ramos <fabiowilliam466@gmail.com>
2024-09-08 12:48:47 -0400

    teste

--- program.cpp
+++ program.cpp
@@ -7,9 +7,9 @@ int main()
     int inc = 2000, fim = 30000, stp = 2000, rpt = 20;
 
     vetorAleatorio(inc, fim, stp, rpt);
-    // vetorReverso(inc, fim, stp);
-    // vetorOrdenado(inc, fim, stp);
-    // vetorQuaseOrdenado(inc, fim, stp);
+    vetorReverso(inc, fim, stp);
+    vetorOrdenado(inc, fim, stp);
+    vetorQuaseOrdenado(inc, fim, stp);
 
     return 0;
 }
